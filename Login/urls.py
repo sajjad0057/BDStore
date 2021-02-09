61ea611a6a13ec3bfe79f11daf8fd8525b2a5dc2
sajjad Hossain <sajjadhossain0057@gamil.com>
2021-02-09 22:38:33 +0600

    Create signin and signup view

--- Login/urls.py
+++ Login/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-
+from . import views
 
 app_name = "Login"  # For Relative url
 
 urlpatterns = [
-    
+    path('signup/',views.signUp,name="signup"),
+    path('login/',views.loginUser,name="login")
 ]
--- Login/views.py
+++ Login/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponseRedirect,HttpResponse
+from .models import Profile
+from .forms import ProfileForm,SignUpForm
+from django.urls import reverse
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login,logout
+
+
 
 # Create your views here.
+
+
+def signUp(request):
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('Login:login'))
+    return render(request,'Login/SignUp.html',context={'form':form})
+
+
+def loginUser(request):
+    form = AuthenticationForm()
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            print("username--->",username)
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username,password=password)
+            if user is not None:
+                login(request,user)
+                return HttpResponse("Logged in")
+    #return HttpResponse("Logged in")       
+    return render(request,'Login/Login.html',context={"form":form})
