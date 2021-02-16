5b1cf7286b6bdb3742819ecf75af5a25cf5c1656
sajjad Hossain <sajjadhossain0057@gamil.com>
2021-02-16 22:35:34 +0600

    Product details view and page

--- Shop/urls.py
+++ Shop/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "Shop"
 
 urlpatterns = [
-    path('',views.Home.as_view(),name='home')
+    path('',views.Home.as_view(),name='home'),
+    path('details/<pk>/',views.productDetail.as_view(),name='details'),
 ]
--- Shop/views.py
+++ Shop/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView,DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Product,Category
+
 
 # Create your views here.
 
@@ -9,3 +11,12 @@ class Home(ListView):
     model = Product
     context_object_name = 'product_list'
     template_name='Shop/home.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(category=Category.objects.all())
+        return context
+
+
+class productDetail(LoginRequiredMixin,DetailView):
+    model = Product
+    template_name = 'Shop/product_details.html'
