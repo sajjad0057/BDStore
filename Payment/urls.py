08aadc7e6e461f860b3277c049c63b8017900e22
sajjad Hossain <sajjadhossain0057@gamil.com>
2021-02-21 03:45:30 +0600

    setup SSLCOMMERZ API

--- Payment/urls.py
+++ Payment/urls.py
@@ -5,6 +5,7 @@ app_name = "Payment"
 
 urlpatterns = [
     path('checkout/',views.checkout,name="checkout"),
-    path('payment/',views.payment,name="payment"),
+    path('process/',views.payment,name="payment"),
+    path('status/',views.complete,name='payment_status'),
 
 ]
--- Payment/views.py
+++ Payment/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,HttpResponseRedirect,redirect
+from django.urls import reverse
 from Order.models import Order
 from .models import BillingAddress
 from .forms import BillingForm
@@ -45,7 +46,56 @@ def payment(request):
         messages.info(request,"Please complete your Info first !")
         return redirect('Login:profile')
     
-    return render(request,'Payment/payment.html',{})
+    # For SSLCOMMERZ API :
+    
+    store_id = 'sajja5f61279af322c'
+    API_key ='sajja5f61279af322c@ssl'
+    mypayment = SSLCSession(sslc_is_sandbox=True, sslc_store_id=store_id, sslc_store_pass=API_key)
+
+    print('request---->',request)
+    status_url = request.build_absolute_uri(reverse('Payment:payment_status')) # Returns the absolute URI form of location. If no location is provided, the location will be set to request.get_full_path().
+    
+    print(status_url)
+    mypayment.set_urls(success_url=status_url, fail_url=status_url,
+                        cancel_url=status_url, ipn_url=status_url)
+
+    order_qs = Order.objects.filter(user=request.user,ordered=False)[0] # if use get instead of filter ,don't need to be indexing
+    orderItems = order_qs.orders_item.all()
+    orderItemsCount = order_qs.orders_item.count()
+    orderTotals = order_qs.get_total_items()
+    mypayment.set_product_integration(total_amount=Decimal(orderTotals),
+                                      currency='BDT', product_category='Mixed',
+                                      product_name=orderItems, num_of_item=orderItemsCount,
+                                      shipping_method='Courier', product_profile='None')
+
+    
+    current_user = request.user
+    
+    mypayment.set_customer_info(name=current_user.profile.full_name, email=current_user.email,
+                                address1=current_user.profile.address_1,
+                                address2=current_user.profile.address_1, city=current_user.profile.city,
+                                postcode=current_user.profile.zipcode, country=current_user.profile.country,
+                                phone=current_user.profile.phone)
+    #print("saved_address.user.profile.full_name ---->>>>>",saved_address.user.profile.full_name)
+    mypayment.set_shipping_info(shipping_to=saved_address.user.profile.full_name,
+                                address=saved_address.address,
+                                city=saved_address.city,
+                                postcode=saved_address.zipcode,
+                                country=saved_address.country)
+
+    # If you want to post some additional values
+    mypayment.set_additional_values(value_a='cusotmer@email.com', value_b='portalcustomerid', value_c='1234', value_d='uuid')
+
+    response_data = mypayment.init_payment()
+    print("response_data ***+++***>>",response_data)
+    
+    return redirect(response_data['GatewayPageURL'])
+
+
+
+@login_required
+def complete(request):
+    return render(request,"Payment/complete.html",)
         
     
 
